1c886537db3961cad111768a11c7e539c3b3a0e9
carolhcs <carol_lgl@hotmail.com>
2017-01-27 19:20:25 -0200

    Codigos usados no prototipo FDEC
    Codigos usados no desenvolvimento do prototipo de jogo flappy dragon eat
    coin criado usando o motor unity e a linguagem de programação c#

---
+++ ALGC_Prototipo_FDEC_CameraSegueJogador.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ALGC_Prototipo_FDEC_CameraSegueJogador : MonoBehaviour {
+	//Esse código faz a camera seguir um trajeto
+	public Transform trajeto;
+	void LateUpdate () {
+		transform.position = new Vector3(trajeto.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ ALGC_Prototipo_FDEC_MovimentaInimigo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ALGC_Prototipo_FDEC_MovimentaInimigo : MonoBehaviour {
+
+	//Movimenta inimigos para cima e para baixo
+	// Velocidade de movimento (0 parado)
+	public float speed = 1;
+	// Switch Movement Direction every x seconds
+	public float switchTime = 2;
+	void Start() {
+		// Direção de movimento inicial
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+		// Switch every few seconds
+		InvokeRepeating("Switch", 0, switchTime);
+	}
+	void Switch() {
+		GetComponent<Rigidbody2D>().velocity *= -2;
+	}
+}
