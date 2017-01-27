443a211c2d605949550fb8bb742a6de2f2ff22fe
carolhcs <carol_lgl@hotmail.com>
2017-01-27 19:40:25 -0200

    Codigos do prototipo FDEC
    Codigos usados no desenvolvimento do prototipo de jogo flappy dragon eat
    coin desenvolvido no unity na linguagem de programção c#

---
+++ ALGC_Prototipo_FDEC_codes/ALGC_Prototipo_FDEC_MudaCena.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ALGC_Prototipo_FDEC_MudaCena : MonoBehaviour {
+
+	public void btJogar (){
+		SceneManager.LoadScene ("02Jogo");
+	}
+	public void btVoltar (){
+		SceneManager.LoadScene ("01MenuPrincipal");
+	}
+	public void btSobre (){
+		SceneManager.LoadScene ("03Sobre");
+	}
+	public void btRanking (){
+		SceneManager.LoadScene ("04Ranking");
+	}
+	public void btSair (){
+		Application.Quit ();
+	}
+	public void btFacebook (){
+		Application.OpenURL("https://www.facebook.com/algamecode/");
+	}
+	public void btTwitter (){
+		Application.OpenURL("https://twitter.com/carol_HCS");
+	}
+	public void btBlogger (){
+		Application.OpenURL("http://algamecode.blogspot.com.br/");
+	}
+}
